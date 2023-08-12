f948e7c7e7f1a2fdbf05ff8dad5a894317dfd677
Aitor Alonso <aitor@aalonso.dev>
2023-08-12 13:44:25 +0200

    test: better handler tests

--- internal/platform/server/handler/zones/list_prices_zones_test.go
+++ internal/platform/server/handler/zones/list_prices_zones_test.go
@@ -1,6 +1,7 @@
 package zones
 
 import (
+	"errors"
 	pvpc "go-pvpc/internal"
 	"go-pvpc/internal/listing"
 	"go-pvpc/internal/platform/storage/storagemocks"
@@ -14,7 +15,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_ListZonesHandler(t *testing.T) {
+func Test_ListZonesHandler_Success(t *testing.T) {
 	gin.SetMode(gin.TestMode)
 	repositoryMock := new(storagemocks.PricesZonesRepository)
 	listingService := listing.NewListingService(repositoryMock)
@@ -22,36 +23,89 @@ func Test_ListZonesHandler(t *testing.T) {
 	r := gin.New()
 	r.GET("/zones", ListZonesHandler(listingService))
 
-	t.Run("list available zones and returns 200", func(t *testing.T) {
-		zone1, err := pvpc.NewPricesZone(pvpc.PricesZoneDto{
-			ID:         "ABC",
-			ExternalID: "1234",
-			Name:       "zone1",
-		})
-		require.NoError(t, err)
-
-		zone2, err := pvpc.NewPricesZone(pvpc.PricesZoneDto{
-			ID:         "DEF",
-			ExternalID: "5678",
-			Name:       "zone2",
-		})
-		require.NoError(t, err)
-
-		repositoryMock.On(
-			"GetAll",
-			mock.Anything,
-		).Return([]pvpc.PricesZone{zone1, zone2}, nil)
-
-		req, err := http.NewRequest(http.MethodGet, "/zones", nil)
-		require.NoError(t, err)
-
-		rec := httptest.NewRecorder()
-		r.ServeHTTP(rec, req)
-
-		res := rec.Result()
-		defer res.Body.Close()
-
-		require.Equal(t, http.StatusOK, res.StatusCode)
-		snaps.MatchSnapshot(t, rec.Body.String())
+	zone1, err := pvpc.NewPricesZone(pvpc.PricesZoneDto{
+		ID:         "ABC",
+		ExternalID: "1234",
+		Name:       "zone1",
 	})
+	require.NoError(t, err)
+
+	zone2, err := pvpc.NewPricesZone(pvpc.PricesZoneDto{
+		ID:         "DEF",
+		ExternalID: "5678",
+		Name:       "zone2",
+	})
+	require.NoError(t, err)
+
+	repositoryMock.On(
+		"GetAll",
+		mock.Anything,
+	).Return([]pvpc.PricesZone{zone1, zone2}, nil)
+
+	req, err := http.NewRequest(http.MethodGet, "/zones", nil)
+	require.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	r.ServeHTTP(rec, req)
+
+	res := rec.Result()
+	defer res.Body.Close()
+
+	repositoryMock.AssertExpectations(t)
+	require.Equal(t, http.StatusOK, res.StatusCode)
+	snaps.MatchSnapshot(t, rec.Body.String())
+
+}
+
+func Test_ListZonesHandler_Empty(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	repositoryMock := new(storagemocks.PricesZonesRepository)
+	listingService := listing.NewListingService(repositoryMock)
+
+	r := gin.New()
+	r.GET("/zones", ListZonesHandler(listingService))
+
+	repositoryMock.On(
+		"GetAll",
+		mock.Anything,
+	).Return([]pvpc.PricesZone{}, nil)
+
+	req, err := http.NewRequest(http.MethodGet, "/zones", nil)
+	require.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	r.ServeHTTP(rec, req)
+
+	res := rec.Result()
+	defer res.Body.Close()
+
+	require.Equal(t, http.StatusOK, res.StatusCode)
+	snaps.MatchSnapshot(t, rec.Body.String())
+}
+
+func Test_ListZonesHandler_Error(t *testing.T) {
+	gin.SetMode(gin.TestMode)
+	repositoryMock := new(storagemocks.PricesZonesRepository)
+	listingService := listing.NewListingService(repositoryMock)
+
+	r := gin.New()
+	r.GET("/zones", ListZonesHandler(listingService))
+
+	repositoryMock.On(
+		"GetAll",
+		mock.Anything,
+	).Return(nil, errors.New("mock error"))
+
+	req, err := http.NewRequest(http.MethodGet, "/zones", nil)
+	require.NoError(t, err)
+
+	rec := httptest.NewRecorder()
+	r.ServeHTTP(rec, req)
+
+	res := rec.Result()
+	defer res.Body.Close()
+
+	require.Equal(t, http.StatusInternalServerError, res.StatusCode)
+	snaps.MatchSnapshot(t, rec.Body.String())
+
 }
