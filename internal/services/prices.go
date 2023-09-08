2d5757c85fcca825baefdbd9c9aa7539d76de2ac
Aitor Alonso <aitor@aalonso.dev>
2023-09-08 20:59:48 +0200

    wip: prices service

--- internal/services/prices.go
+++ internal/services/prices.go
@@ -3,6 +3,7 @@ package services
 import (
 	"context"
 	"errors"
+	"time"
 
 	"pvpc-backend/internal/domain"
 )
@@ -10,25 +11,49 @@ import (
 // PricesService is the domain service that manages operations over Price's.
 type PricesService struct {
 	pricesRepository domain.PricesRepository
+	zonesRepository  domain.ZonesRepository
 }
 
 // NewPricesService returns a new ListingService.
-func NewPricesService(pricesRepository domain.PricesRepository) PricesService {
+func NewPricesService(pricesRepository domain.PricesRepository, zonesRepository domain.ZonesRepository) PricesService {
 	return PricesService{
 		pricesRepository: pricesRepository,
+		zonesRepository:  zonesRepository,
 	}
 }
 
 // FetchAndStorePricesFromREE calls REE APIs to fetch prices and stores them in the database.
 func (s PricesService) FetchAndStorePricesFromREE(ctx context.Context) error {
-	/*
-		1. Get last prices from database
-		2. Check if last prices date is less than today
-		3. If true, fetch today prices from REE
-		4. Store prices in database
-		5. Check if time is before 20:30
-		6. If true, fetch tomorrow prices from REE
-		7. Store prices in database
-	*/
+	var zonesToFetchToday []domain.Zone
+	var zonesToFetchTomorrow []domain.Zone
+
+	prices, err := s.pricesRepository.Query(ctx, nil, nil)
+	if err != nil {
+		return err
+	}
+
+	today := time.Now()
+
+	if today.Hour() > 20 {
+		zonesToFetchTomorrow, err = s.zonesRepository.GetAll(ctx)
+		if err != nil {
+			return err
+		}
+	}
+
+	today = today.Truncate(24 * time.Hour)
+
+	for _, price := range prices {
+		if price.Date().Before(today) {
+			zonesToFetchToday = append(zonesToFetchToday, price.Zone())
+		}
+	}
+
+	// TODO: Fetch prices from REE
+	// function that receives zonesToFetchToday and zonesToFetchTomorrow and returns a slice of prices
+
+	// TODO: Store prices in database
+	// Store the prices slice returned by the previous function into database
+
 	return errors.New("not implemented")
 }
