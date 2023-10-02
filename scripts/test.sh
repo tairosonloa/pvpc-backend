2c007bdf4302a1c1dd884916b0663cf0caa17dd7
Aitor Alonso <aitor@aalonso.dev>
2023-10-02 20:17:15 +0200

    test: sets coverage to 80%

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,7 @@ set -e -u
 go test ./... -coverprofile coverage.out -covermode atomic
 
 perc=`go tool cover -func=coverage.out | tail -n 1 | sed -Ee 's!^[^[:digit:]]+([[:digit:]]+(\.[[:digit:]]+)?)%$!\1!'`
-res=`echo "$perc >= 90.0" | bc` # 90% minimum coverage
-test "$res" -eq 1 && echo "OK: Coverage of $perc % (threshold requires >= 90.0 %)" && exit 0
+res=`echo "$perc >= 80.0" | bc` # 80% minimum coverage
+test "$res" -eq 1 && echo "OK: Coverage of $perc % (threshold requires >= 80.0 %)" && exit 0
 echo "Insufficient coverage: $perc" >&2
 exit 1
